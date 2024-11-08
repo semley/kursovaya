71c983b59c77ebe5597dc7cd4e9144fdbe5dc821
Adelina Semenova <adalyk@yandex.ru>
2024-11-08 21:21:49 +0500

    fullproekt

---
+++ Kursach/Kursach/AddEditPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursach
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Faculty _currentServise = new Faculty();
+
+        public bool a = false;
+
+        public AddEditPage(Faculty SelectedService)
+        {
+            InitializeComponent();
+            if (SelectedService != null)
+            {
+                a = true;
+                _currentServise = SelectedService;
+            }
+
+            DataContext = _currentServise;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentServise.Faculty_name))
+            {
+                errors.AppendLine("Укажите название факультета");
+            }
+            if (string.IsNullOrWhiteSpace(_currentServise.FIO_decan))
+            {
+                errors.AppendLine("Укажите ФИО декана");
+            }
+            if (string.IsNullOrWhiteSpace(_currentServise.Phone))
+            {
+                errors.AppendLine("Укажите номер телефона");
+            }
+            if (string.IsNullOrWhiteSpace(_currentServise.Num_room))
+            {
+                errors.AppendLine("Укажите номер кабинета");
+            }
+            if (string.IsNullOrWhiteSpace(_currentServise.Phone) || !Regex.IsMatch(_currentServise.Phone, @"^\d{11}$")) // Проверка на 11 цифр
+            {
+                errors.AppendLine("Укажите номер телефона, состоящий из 11 цифр");
+            }
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            var allServices = Semenova_UniversityKursachEntities.GetContext().Faculty.ToList();
+
+            // Фильтруем по имени факультета, исключая текущий факультет (при редактировании)
+            allServices = allServices.Where(p => p.Faculty_name == _currentServise.Faculty_name && p.Faculty_ID != _currentServise.Faculty_ID).ToList();
+
+            // Проверяем, есть ли уже факультет с таким именем, кроме текущего (при редактировании)
+            if (allServices.Count == 0 || _currentServise.Faculty_ID != 0)
+            {
+                if (_currentServise.Faculty_ID == 0)
+                {
+                    // Находим максимальный ID существующего факультета:
+                    int maxId = Semenova_UniversityKursachEntities.GetContext().Faculty.Max(f => f.Faculty_ID);
+                    // Увеличиваем максимальный ID на 1, чтобы получить новый ID:
+                    _currentServise.Faculty_ID = maxId + 1;
+                    Semenova_UniversityKursachEntities.GetContext().Faculty.Add(_currentServise);
+                }
+                else
+                {
+                    // Обновление существующего факультета
+                    Semenova_UniversityKursachEntities.GetContext().Entry(_currentServise).State = EntityState.Modified;
+                }
+                // 3. Добавляем новый факультет в базу данных или обновляем существующий:
+                
+
+                try
+                {
+                    Semenova_UniversityKursachEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена");
+                    Manager.MainFrame.GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Уже существует такой факультет");
+            }
+        }
+
+        private void ChangePictureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+            if (myOpenFileDialog.ShowDialog() == true)
+            {
+                _currentServise.Image = myOpenFileDialog.FileName;
+                LogoImage.Source = new BitmapImage(new Uri(myOpenFileDialog.FileName));
+            }
+        }
+    }
+}
---
+++ Kursach/Kursach/FacultyPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursach
+{
+    /// <summary>
+    /// Логика взаимодействия для PulpitPage.xaml
+    /// </summary>
+    public partial class FacultyPage : Page
+    {
+        public bool a = false;
+        int CountRecords;
+        int CountPage;
+        int CurrentPage;
+        List<Faculty> CurrentPageList = new List<Faculty>();
+        List<Faculty> TableList;
+
+        public int vsecurrentProductCount = Semenova_UniversityKursachEntities.GetContext().Faculty.Count();
+        public FacultyPage()
+        {
+            InitializeComponent();
+            var currentServices = Semenova_UniversityKursachEntities.GetContext().Faculty.ToList();
+            ServiceListView.ItemsSource = currentServices;
+            Update();
+        }
+
+
+        public void Update()
+        {
+            var currentServices = Semenova_UniversityKursachEntities.GetContext().Faculty.ToList();
+
+            if (SortBox.SelectedIndex == 1)
+            {
+                currentServices = currentServices.OrderBy(p => p.Faculty_name).ToList();
+            }
+            else if (SortBox.SelectedIndex == 2)
+            {
+                currentServices = currentServices.OrderByDescending(p => p.Faculty_name).ToList();
+            }
+
+            if (FiltrBox.SelectedIndex == 0)
+            {
+                currentServices = currentServices.Where(p => (p.PulpitCount >= 0 )).ToList();
+            }
+            if (FiltrBox.SelectedIndex == 1)
+            {
+                currentServices = currentServices.Where(p => (p.PulpitCount >= 0 && p.PulpitCount < 3)).ToList();
+            }
+            if (FiltrBox.SelectedIndex == 2)
+            {
+                currentServices = currentServices.Where(p => (p.PulpitCount >= 3 && p.PulpitCount < 6)).ToList();
+            }
+            if (FiltrBox.SelectedIndex == 3)
+            {
+                currentServices = currentServices.Where(p => (p.PulpitCount >= 6)).ToList();
+            }
+
+            currentServices = currentServices.Where(p => p.Faculty_name.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            ServiceListView.ItemsSource = currentServices;
+
+
+            ServiceListView.ItemsSource = currentServices;
+            int currentProductCount = currentServices.Count();
+            vsecurrentProductCount = Semenova_UniversityKursachEntities.GetContext().Faculty.Count();
+            CountTB.Text = "кол-во " + (currentProductCount).ToString() + " из " + (vsecurrentProductCount).ToString();
+
+
+
+            
+
+        }
+
+
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Update();
+        }
+
+        private void FiltrBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+        }
+
+        private void SortBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+        }
+
+
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            a = false;
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            a = true;
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Faculty));
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentService = (sender as Button).DataContext as Faculty;
+
+            var currentClientServices = Semenova_UniversityKursachEntities.GetContext().Pulpit.ToList();
+            currentClientServices = currentClientServices.Where(p => p.Pulpit_ID == currentService.Faculty_ID).ToList();
+
+            if (currentClientServices.Count > 0)
+                MessageBox.Show("Невозможно выполнить удаление, так как существуют кафедры на факультете");
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Semenova_UniversityKursachEntities.GetContext().Faculty.Remove(currentService);
+                        Semenova_UniversityKursachEntities.GetContext().SaveChanges();
+                        ServiceListView.ItemsSource = Semenova_UniversityKursachEntities.GetContext().Faculty.ToList();
+                        Update();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+
+            }
+        }
+            
+
+
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                Semenova_UniversityKursachEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = Semenova_UniversityKursachEntities.GetContext().Faculty.ToList();
+            }
+            Update();
+        }
+
+        
+    }
+}
